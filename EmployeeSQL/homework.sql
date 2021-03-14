56f16e5061496280c1d5f9f3de27d745cb1eed34
E-Weber <ewebersms@gmail.com>
2021-03-14 15:58:16 -0500

    second q

--- EmployeeSQL/homework.sql
+++ EmployeeSQL/homework.sql
@@ -2,7 +2,8 @@
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary from employees
 inner join salaries on employees.emp_no = salaries.emp_no;
 --2)List first name, last name, and hire date for employees who were hired in 1986.
-
+select emp_no, first_name, hire_date from employees
+where hire_date >= '1986-01-01' and hire_date <= '1986-12-31'
 
 --3)List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
